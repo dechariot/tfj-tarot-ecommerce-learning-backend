bafce78f3650afd2dc6aed62be5dcb542d9ef2e1
dechariot <nsprisang@gmail.com>
2020-05-27 23:45:49 +0700

    Add category model, controller and route ; create category

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 //IMPORT ROUTES
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 
 //APP
@@ -38,6 +39,7 @@ app.use(expressValidator());
 //ROUTES middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
 
